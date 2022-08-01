7ba193c5fcd5865f32c54ff0d8b3d1416030648a
Mateus Cristofori <mateuscristofori@gmail.com>
2022-08-01 17:47:14 -0300

    Adicionando dados na API

--- secao7/src/App.js
+++ secao7/src/App.js
@@ -6,6 +6,9 @@ function App() {
   const [products, setProducts] = useState([])
   const url = 'http://localhost:3000/products'
 
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
   // 1 - resgatando dados.
   useEffect(() => {
     async function fetchData() {
@@ -18,16 +21,58 @@ function App() {
     fetchData()
   }, [])
 
+  // 2 - adição de produtos
+
+  const handleSubmit = async evento => {
+    evento.preventDefault()
+
+    const product = {
+      name: name,
+      price: price
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json'
+      },
+      body: JSON.stringify(product)
+    })
+  }
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
       <ul>
         {products.map(product => (
           <li key={product.id}>
-            {product.name} - {product.price}
+            {product.name} - R$: {product.price}
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={evento => setName(evento.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={evento => setPrice(evento.target.value)}
+            />
+          </label>
+          <input type="submit" value="enviar" />
+        </form>
+      </div>
     </div>
   )
 }
